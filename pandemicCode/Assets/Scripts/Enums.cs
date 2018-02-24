d692e8c435232d889ca32a3a45f29fabeb9ff832
Dan Ning <ydn2012@hotmail.com>
2018-02-24 16:01:43 -0500

    Pandemic 0.0.3
    Add cityNames to Enums class

--- pandemicCode/Assets/Scripts/Enums.cs
+++ pandemicCode/Assets/Scripts/Enums.cs
@@ -10,7 +10,56 @@ public class Enums : MonoBehaviour {
     public enum GamePhase { ReadyToJoin,SetupGame,PlayerTakeTurn,InfectCities,PlayerDrawCard,Completed};
     public enum RoleKind { ContingencyPlanner, Dispatcher, Medic, OperationsExpert, QuarantineSpecialist, Researcher, Scientist,Archivist};
     public enum EventKind{ResilientPopulation, Airlift, OneQuietNight, Forecast, GovernmentGrant };
-    public enum CityName { Algiers, Atlanta, Baghdad, Bangkok, Beijing, Bogota, Cairo, BuenosAires, Chennai, Chicago, Washington, Miami};
+    public enum CityName {
+        Algiers,
+Atlanta,
+Baghdad,
+Bangkok,
+Beijing,
+Bogota,
+BuenosAries,
+Cairo,
+Chennai,
+Chicago,
+Delhi,
+Essen,
+HoChiMinhCity,
+HongKong,
+Istanbul,
+Jakarta,
+Johannesburg,
+Karachi,
+Khartoum,
+Kinshasa,
+Kolkata,
+Lagos,
+Lima,
+London,
+LosAngeles,
+Madrid,
+Manila,
+MexicoCity,
+Miami,
+Milan,
+Montreal,
+Moscow,
+Mumbai,
+NewYork,
+Osaka,
+Paris,
+Riyadh,
+SanFrancisco,
+Santiago,
+SaoPaulo,
+Seoul,
+Shanghai,
+StPetersburg,
+Sydney,
+Taipei,
+Tehran,
+Tokyo,
+Washington
+    };
     public enum Challenge { Nochallenge, Mutation, BioTerroist,VirulentStrain, MutationAddVirulentStrain,VirulentStrainAddBioTerroist};
     public enum CardType {CityCard, EpidemicCard, EventCard, InfectionCard };
 }
