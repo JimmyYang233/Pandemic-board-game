cfc301340a6e0761856fa2f95f1bb415892ec849
Bobo <bowen.tian@mail.mcgill.ca>
2018-02-24 19:43:03 -0500

    Game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
+	private Enums.Challenge challenge;
+	private Enums.GamePhase currentPhase;
+	private boolean hasDLC;
+	private int infectionRate;
+	private int[] infectionArray;
+	private int infectionCardDrawn;
+	private boolean isnewGame;
+	private int outbreaksValue;
+	private int researchStationRemain;
+	private boolean resolvingEpidemic;
 
-	// Use this for initialization
-	void Start () {
-		
+	private Player currentPlayer;
+	private List<Player> players = new List<Player>();
+	private List<InfectionCard> infectionDeck = new List<InfectionCard>();
+	private List<InfectionCard> infectionDiscardPile = new List<InfectionCard>();
+	private List<City> outbreakedCities = new List<City>();
+	private List<City> cities = new List<City>();
+
+	public Game(int numOfPlayer, int numOfEpidemicCard, List<User> users){
+
 	}
+
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
