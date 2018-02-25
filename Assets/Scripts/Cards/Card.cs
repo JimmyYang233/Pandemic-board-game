69770ca8e13f09606b4bf2e3e6602b4aa1141eb5
Dan Ning <ydn2012@hotmail.com>
2018-02-24 23:44:52 -0500

    Move Ui script folder from Asset to scripts

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Card
+{
+    Enums.CardType getType();
+}
--- Assets/Scripts/City.cs
+++ Assets/Scripts/City.cs
@@ -140,7 +140,7 @@ public class City : MonoBehaviour {
     public void setButtonActive()
     {
         Button theButton = gameObject.GetComponent<Button>();
-        theButton.GetComponent<Button>().interactable = false;
+        theButton.GetComponent<Button>().interactable = true;
     }
     
 }
