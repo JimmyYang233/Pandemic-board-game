a09288148392333574a38b9cd4d3b454fdb01872
Bowen Tian <bowen.tian@mail.mcgill.ca>
2018-03-04 12:13:19 -0500

    Epidemic singleton

--- Assets/Scripts/Cards/EpidemicCard.cs
+++ Assets/Scripts/Cards/EpidemicCard.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EpidemicCard : PlayerCard {
-    public EpidemicCard() : base(CardType.EpidemicCard)
+    private EpidemicCard() : base(CardType.EpidemicCard)
     {
+    }
+
+    private static EpidemicCard INSTANCE = new EpidemicCard();
+
+    public EpidemicCard getEpidemicCard()
+    {
+        return INSTANCE;
     }
 }
