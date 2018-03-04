b5d3c68423e982d405c1ab1effdb5bd1669fd4fe
Bowen Tian <bowen.tian@mail.mcgill.ca>
2018-03-04 12:15:18 -0500

    epidemicCard style

--- Assets/Scripts/Cards/EpidemicCard.cs
+++ Assets/Scripts/Cards/EpidemicCard.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EpidemicCard : PlayerCard {
+    private static EpidemicCard INSTANCE = new EpidemicCard();
+
     private EpidemicCard() : base(CardType.EpidemicCard)
     {
     }
-
-    private static EpidemicCard INSTANCE = new EpidemicCard();
-
+    
     public EpidemicCard getEpidemicCard()
     {
         return INSTANCE;
