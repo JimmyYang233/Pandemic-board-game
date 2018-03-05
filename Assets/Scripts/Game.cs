170609f10532a1468dd70e45fa31b354805a9db6
Dan Ning <ydn2012@hotmail.com>
2018-03-05 13:14:31 -0500

    Put every operation from game to a moveOperation.cs

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -336,14 +336,6 @@ public class Game : MonoBehaviour {
     }
 
 
-    /// <summary>
-    /// All below values and operations are only used in the client system. 
-    /// </summary>
-    public Button driveButton;
-    public Button directFlightButton;
-    public Button shuttleFlightButton;
-    public Button charterFlightButton;
-    public Button cancelButton;
     public City testCity;
     public City destinationCity;
     public Pawn testPawn;
@@ -352,79 +344,6 @@ public class Game : MonoBehaviour {
     private static Player testPlayer = new Player(testUser);
 
 
-    //This part for test only
-    void Start() {  
-        
-        Debug.Log("Start");
-
-    }
 
 
-    //All below may stay here forever
-    public void moveButtonClicked()
-    {
-        testRole.setPawn(testPawn);
-        testCity.addPawn(testPawn);
-        //testPlayer.setRole(testRole);
-        //testPlayer.addCard(new CityCard(testCity));
-        // testPlayer.addCard(new CityCard(destinationCity));
-        //currentPlayer = testPlayer;
-        driveButton.GetComponent<Button>().interactable = true;
-        City currentCity = currentPlayer.getPlayerPawn().getCity();
-        
-        if (currentPlayer.containsCityCard())
-        {
-            directFlightButton.GetComponent<Button>().interactable = true;
-        }
-        if (currentPlayer.containsSpecificCityCard(currentCity))
-        {
-            charterFlightButton.GetComponent<Button>().interactable = true;
-        }
-        if (currentCity.getHasResearch())
-        {
-            shuttleFlightButton.GetComponent<Button>().interactable = true;
-        }
-
-
-    }
-    //public City tempCity;
-    public void cancelButtonClicked()
-    {
-        disableAllCities();
-    }
-
-    public void driveButtonClicked()
-    {
-        City currentCity = currentPlayer.getPlayerPawn().getCity();
-        foreach (City neighbor in currentCity.getNeighbors())
-        {
-            neighbor.displayButton();
-        }
-    }
-
-    
-    public void testMovePawn(City destinationCity)
-    {
-        City currentCity = currentPlayer.getPlayerPawn().getCity();
-        Vector3 position = destinationCity.transform.position;
-        position.y = position.y + 10; 
-        testPawn.transform.position = position;
-        disableAllCities();
-    }
-
-    public static void disableAllCities()
-    {
-        GameObject[] tmp = GameObject.FindGameObjectsWithTag("City");
-        foreach (GameObject aObject in tmp)
-        {
-            Button button = aObject.GetComponent<Button>();
-            if (button.interactable)
-            {
-                button.interactable = false;
-            }
-        }
-    }
-
-    
-    
 }
