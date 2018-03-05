c620e86ce330e0d8edbcb1fb1bf022206097c218
Shihang Zhu <shihang.zhu@mail.mcgill.ca>
2018-03-05 16:28:37 -0500

    roleColors finished

--- Assets/Scripts/Maps.cs
+++ Assets/Scripts/Maps.cs
@@ -26,12 +26,27 @@ public class Maps : MonoBehaviour {
         playerDescriptions.Add(RoleKind.Generalist, "The Generalist may do up to 5 actions each turn.");
         playerDescriptions.Add(RoleKind.Troubleshooter, "The Troubleshooter, at the start of her turn, looks at as many Infection cards as the current infection rate (by taking them from the top of the Infection Deck, looking at them, and putting them back in the same order).");
 
-        //TO-DO
-        diseaseColor.Add(Color.black);
 
-        //TO-DO
-        roleColors.Add(RoleKind.Scientist, Color.gray);
+        diseaseColor.Add(Color.black);
+        diseaseColor.Add(Color.red);
+        diseaseColor.Add(Color.blue);
+        diseaseColor.Add(Color.yellow);
+
+       // Colors are compared and chosen from http://www.99colors.net/dot-net-colors
+        roleColors.Add(RoleKind.ContingencyPlanner, Color.mediumturquoise);
+        roleColors.Add(RoleKind.Dispatcher, Color.orchid);
+        roleColors.Add(RoleKind.Medic, Color.orange);
+        roleColors.Add(RoleKind.OperationsExpert, Color.springgreen);
+        roleColors.Add(RoleKind.QuarantineSpecialist, Color.limegreen);
+        roleColors.Add(RoleKind.Researcher, Color.sienna);
+        roleColors.Add(RoleKind.Scientist, Color.ghostwhite);
         roleColors.Add(RoleKind.Archivist, Color.blue);
+        roleColors.Add(RoleKind.ContainmentSpecialist, Color.wheat);
+        roleColors.Add(RoleKind.Epidemiologist, Color.pink);
+        roleColors.Add(RoleKind.FieldOperative, Color.yellow);
+        roleColors.Add(RoleKind.Generalist, Color.gray);
+        roleColors.Add(RoleKind.Troubleshooter, Color.red);
+
 
 // origin 5 eventcard
         eventCardDescription.Add(EventKind.Airlift, "Move a pawn to any city. If played on another player's pawn you must have permission to move it.");
