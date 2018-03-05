7fbbd676b4eb0708d1a30a5edb60a0d8fad45c16
Dan Ning <ydn2012@hotmail.com>
2018-03-05 02:09:54 -0500

    我感觉很难受。。。

--- Assets/Scripts/UI scripts/Scroll.cs
+++ Assets/Scripts/UI scripts/Scroll.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Scroll : MonoBehaviour {
-
     public float speed = 0.5f;
     Vector3 startPos;
 	// Use this for initialization
@@ -14,11 +13,13 @@ public class Scroll : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(new Vector3(-1, 0, 0) * speed * Time.time);
+
+        transform.Translate(new Vector3(-1, 0, 0) * speed);
         if(transform.position.x < -557)
         {
             transform.position = startPos;
         }
         
 	}
+
 }
---
+++ Assets/Scripts/UI scripts/cityScroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cityScroll : MonoBehaviour {
+    Vector3 startPos;
+    // Use this for initialization
+    void Start () {
+        startPos = transform.position;
+        //Debug.Log(gameObject.name + "" + transform.position.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -278.5)
+        {
+            //Debug.Log(transform.parent.transform.position.x);
+            //Debug.Log(gameObject.name + "" + transform.position.x);
+            transform.position = new Vector3(transform.position.x + 557, transform.position.y, transform.position.z);
+        }
+        else if (transform.parent.transform.position.x == 0)
+        {
+            Debug.Log(transform.parent.transform.position.x);
+            transform.position = startPos;
+        }
+    }
+}
