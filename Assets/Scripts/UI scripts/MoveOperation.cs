4b298dd3e2227bbcc43b25453947539563983f29
Dan Ning <ydn2012@hotmail.com>
2018-03-05 13:21:50 -0500

    Few Changes

--- Assets/Scripts/UI scripts/MoveOperation.cs
+++ Assets/Scripts/UI scripts/MoveOperation.cs
@@ -13,12 +13,6 @@ public class MoveOperation : MonoBehaviour {
 
     public void moveButtonClicked()
     {
-        testRole.setPawn(testPawn);
-        testCity.addPawn(testPawn);
-        //testPlayer.setRole(testRole);
-        //testPlayer.addCard(new CityCard(testCity));
-        // testPlayer.addCard(new CityCard(destinationCity));
-        //currentPlayer = testPlayer;
         driveButton.GetComponent<Button>().interactable = true;
         City currentCity = currentPlayer.getPlayerPawn().getCity();
 
@@ -34,6 +28,8 @@ public class MoveOperation : MonoBehaviour {
         {
             shuttleFlightButton.GetComponent<Button>().interactable = true;
         }
+    }
+        
 
         /// <summary>
         /// All below values and operations are only used in the client system. 
@@ -52,42 +48,42 @@ public class MoveOperation : MonoBehaviour {
 
     //All below may stay here forever
 
-}
 //public City tempCity;
-public void cancelButtonClicked()
-{
-    disableAllCities();
-}
-
-public void driveButtonClicked()
-{
-    City currentCity = currentPlayer.getPlayerPawn().getCity();
-    foreach (City neighbor in currentCity.getNeighbors())
+    public void cancelButtonClicked()
     {
-        neighbor.displayButton();
+        disableAllCities();
     }
-}
 
-
-public void testMovePawn(City destinationCity)
-{
-    City currentCity = currentPlayer.getPlayerPawn().getCity();
-    Vector3 position = destinationCity.transform.position;
-    position.y = position.y + 10;
-    testPawn.transform.position = position;
-    disableAllCities();
-}
-
-public static void disableAllCities()
-{
-    GameObject[] tmp = GameObject.FindGameObjectsWithTag("City");
-    foreach (GameObject aObject in tmp)
+    public void driveButtonClicked()
     {
-        Button button = aObject.GetComponent<Button>();
-        if (button.interactable)
+        City currentCity = currentPlayer.getPlayerPawn().getCity();
+        foreach (City neighbor in currentCity.getNeighbors())
         {
-            button.interactable = false;
+            neighbor.displayButton();
         }
     }
-}
+
+
+    public void testMovePawn(City destinationCity)
+    {
+        City currentCity = currentPlayer.getPlayerPawn().getCity();
+        Vector3 position = destinationCity.transform.position;
+        position.y = position.y + 10;
+        testPawn.transform.position = position;
+        disableAllCities();
     }
+
+    public static void disableAllCities()
+    {
+        GameObject[] tmp = GameObject.FindGameObjectsWithTag("City");
+        foreach (GameObject aObject in tmp)
+        {
+            Button button = aObject.GetComponent<Button>();
+            if (button.interactable)
+            {
+                button.interactable = false;
+            }
+        }
+    }
+   
+}
