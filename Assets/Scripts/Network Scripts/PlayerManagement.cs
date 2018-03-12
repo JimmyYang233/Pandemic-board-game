2c2b42be97f5cce7dbda38ea423c6aa851dad674
Zhening <taylor_fantasy@163.com>
2018-03-12 17:58:14 -0400

    Add:Photon Player and playerManagement

---
+++ Assets/Scripts/Network Scripts/PlayerManagement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManagement : MonoBehaviour {
+
+	public static PlayerManagement Instance;
+	private PhotonView PhotonView;
+	private List<Player> Players = new List<Player> ();
+	// Use this for initialization
+	private void Awake(){
+		Instance = this;
+		PhotonView = GetComponent<PhotonView> ();
+	}
+	
+	public void AddPlayer(PhotonPlayer photonPlayer){
+		int index = Players.FindIndex (x => x.PhotonPlayer == photonPlayer);
+		if (index == -1) {
+			Players.Add (new Player(photonPlayer));
+		}
+	}
+}
+ 
