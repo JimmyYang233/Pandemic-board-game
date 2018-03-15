1557caefff79a72306737629f86f3df1b2e5c26d
Meowdred <shuranzheng@shurandeMacBook-Pro.local>
2018-03-15 15:52:36 -0400

    playerSelectionPlanel

--- Assets/Scripts/UI scripts/playerSelectionPanel.cs
+++ Assets/Scripts/UI scripts/playerSelectionPanel.cs
@@ -7,11 +7,14 @@ using UnityEngine.EventSystems;
 public class playerSelectionPanel : MonoBehaviour {
 	Maps map;
 	//TODO- other operation
+	public Game game;
 	private enum Status {SHARE,OTHER};
 	public ShareOperation share;
 	private Status selectStatus = Status.SHARE; 
+	public bool[] showOrNot;//to remember whether the spot is used or not  
 	void Awake(){
 		map = Maps.getInstance ();
+		showOrNot = new bool[4];
 	}
 	void Start () {
 		
@@ -31,33 +34,65 @@ public class playerSelectionPanel : MonoBehaviour {
 
     public void addOtherPlayer(RoleKind k)
     {
+		int i = 0;
         foreach (Transform t in this.transform)
         {
+			
             if (!t.gameObject.activeSelf)
             {
+				showOrNot [i] = true;
                 t.gameObject.SetActive(true);
 				t.GetChild(0).GetComponent<Text>().text = k.ToString();
 				t.name = k.ToString ();
 				if (map.getRoleColor(k) != null)
 				{
+					
 
 					t.GetComponent<Image>().color = map.getRoleColor(k);
 				}
 
                 break;
             }
+			i++;
         }
 
         // apply it on current object's material
     }
 	public void characterSelect(){
 		if (selectStatus == Status.SHARE) {
+			reset ();
 			share.selectRole(EventSystem.current.currentSelectedGameObject.name);
 		}
 	}
 	public void cancelButtonClick(){
 		if (selectStatus == Status.SHARE) {
 			share.cancel ();
+		}
+	}
+	//only display player who is in the same city
+	public void displayPlayerNecessary(){
+		foreach (Player p in game.getPlayers()) {
+			if (!p.getPlayerPawn ().getCity ().Equals (game.getCurrentPlayer ().getPlayerPawn ().getCity ())) {
+				concealPlayer (p.getRoleKind());
+			} 
+		}
+	}
+	public void concealPlayer(RoleKind r){
+		foreach (Transform t in this.transform)
+		{
+			if (t.gameObject.active && t.GetChild (0).GetComponent<Text> ().text.Equals (r.ToString ())) {
+				t.gameObject.SetActive (false);
+			}
+		}
+	}
+	public void reset(){
+		int i = 0;
+		foreach (Transform t in this.transform)
+		{
+			if (showOrNot [i]) {
+				t.gameObject.SetActive (true);
+			}
+			i++;
 		}
 	}
 }
