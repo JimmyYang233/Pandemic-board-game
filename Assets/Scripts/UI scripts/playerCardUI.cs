af74b0f212889573671f490de8266326773135a3
Meowdred <shuranzheng@shurandeMacBook-Pro.local>
2018-03-24 22:42:53 -0400

    Chage position

--- Assets/Scripts/UI scripts/playerCardUI.cs
+++ Assets/Scripts/UI scripts/playerCardUI.cs
@@ -7,8 +7,12 @@ public class playerCardUI : MonoBehaviour {
 	Vector3 target;
 	public float speed;
 	private Vector3 addSize;
+	private Vector3 addPosition;
+	private Vector3 originalPosition;
 	private Vector3 originalSize;
+	bool isEventCard=false;
 	public void Awake(){
+		isEventCard = false;
 		start = false;
 		speed = 40;
 		addSize = new Vector3(1.5f,1.5f,1.5f);
@@ -20,22 +24,31 @@ public class playerCardUI : MonoBehaviour {
 			transform.position = Vector3.MoveTowards(transform.position, target, step);
 			if (target == transform.position) {
 				start = false;
+				addPosition = this.transform.position + new Vector3 (0, 40, 0);
+				originalPosition = this.transform.position;
 			}
 		}
 	}
+
 	public void setDestination(Vector3 target){
 		this.target = target;
 		start = true;
 	}
     public void mouseOn()
     {
-        this.transform.position += new Vector3(0,40,0);
-		this.transform.localScale = addSize;
+		if (!start) {
+			this.transform.position = addPosition;
+			this.transform.localScale = addSize;
+		} 
+
     }
 
     public void mouseLeave()
     {
-        this.transform.position += new Vector3(0, -40, 0);
-		this.transform.localScale = originalSize;
+		if (!start) {
+			this.transform.position = originalPosition;
+			this.transform.localScale = originalSize;
+		}
+
     }
 }
