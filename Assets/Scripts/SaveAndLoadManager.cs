1673dc536e08b67b334dd2e224a2b35d4d3f3020
Zhening <taylor_fantasy@163.com>
2018-03-29 09:54:06 -0400

    New Save&Load
    GameData TO BE FINISHED

---
+++ Assets/Scripts/SaveAndLoadManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+using System.IO;
+
+
+public static class SaveAndLoadManager{
+
+	public static void SaveGameData(Game game, string name){
+		BinaryFormatter bf = new BinaryFormatter ();
+		string path = Path.Combine (Application.persistentDataPath, name + ".pandemic");
+		if (File.Exists (path)) {
+			File.Delete (path);
+		}
+		FileStream stream = new FileStream (path, FileMode.Create);
+
+		GameData data = new GameData (game);
+
+		bf.Serialize (stream, data);
+		stream.Close ();
+	}
+
+	public static GameData LoadGameData(string name){
+		string path = Path.Combine (Application.persistentDataPath, name + ".pandemic");
+		if (File.Exists (path)) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream stream = new FileStream (path, FileMode.Open);
+
+			GameData data = bf.Deserialize (stream) as GameData;
+
+			stream.Close ();
+			return GameData;
+		}
+	}
+}
+
+[Serializable]
+public class GameData{
+	
+	public GameData(Game game){
+		
+	}
+}
