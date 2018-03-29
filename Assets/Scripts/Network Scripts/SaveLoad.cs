540785160180a5bee7c19ca1bb8db1cc7a70c974
Zhening <taylor_fantasy@163.com>
2018-03-28 21:26:56 -0400

    Save load test

--- Assets/Scripts/Network Scripts/SaveLoad.cs
+++ Assets/Scripts/Network Scripts/SaveLoad.cs
@@ -4,15 +4,24 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using UnityEngine;
 
-public static class Save{
+public static class SaveLoad{
 	public static List<Game> savedGames = new List<Game>();
 
+	public static void Save() {
+		savedGames.Add(Game.Instance);
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create (Path.Combine(Application.persistentDataPath, "savedGames.gd"));
+		bf.Serialize(file, SaveLoad.savedGames);
+		file.Close();
+	}
+
+	public static void Load() {
+		if(File.Exists(Application.persistentDataPath + "/savedGames.gd")) {
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+			SaveLoad.savedGames = (List<Game>)bf.Deserialize(file);
+			file.Close();
+		}
+	}
 }
 
-public static void Save() {
-	savedGames.Add(Game.current);
-	BinaryFormatter bf = new BinaryFormatter();
-	FileStream file = File.Create (Application.persistentDataPath + "/savedGames.gd");
-	bf.Serialize(file, SaveLoad.savedGames);
-	file.Close();
-}
