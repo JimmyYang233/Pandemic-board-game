89dce15ac4b94fadbe5373d1d7b5d407debc594a
Meowdred <shuranzheng@shurandeMacBook-Pro.local>
2018-03-29 10:08:44 -0400

    eventcard class

---
+++ Assets/Scripts/UI scripts/eventCardUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventCardUI : MonoBehaviour {
+	GameObject eventCardPanel;
+	// Use this for initialization
+	void Start () {
+		eventCardPanel = GameObject.FindGameObjectWithTag ("eventCardAsk");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
