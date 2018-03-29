fe9dba2dc50074e83a9ca8c23d8210b48ccbd70a
Meowdred <shuranzheng@shurandeMacBook-Pro.local>
2018-03-29 10:19:16 -0400

    save change

---
+++ Assets/Scripts/UI scripts/saveController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class saveController : MonoBehaviour {
+	public Text text;
+	public Game game;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void save(){
+		game.save (text.ToString ());
+	}
+}
