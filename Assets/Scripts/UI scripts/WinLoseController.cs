513e578a44a3bda24533eb33cf1c723a5d295bad
Dan Ning <ydn2012@hotmail.com>
2018-04-13 20:09:25 -0400

    win-lose controller finish

---
+++ Assets/Scripts/UI scripts/WinLoseController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLoseController : MonoBehaviour {
+
+    public Game game;
+    public GameObject mainCanavas;
+    public GameObject winPanel;
+    public GameObject losePanel;
+	
+
+    public void notifyWin()
+    {
+        mainCanavas.gameObject.SetActive(true);
+        winPanel.gameObject.SetActive(true);
+    }
+
+    public void notifyLose()
+    {
+        mainCanavas.gameObject.SetActive(true);
+        losePanel.gameObject.SetActive(true);
+    }
+}
