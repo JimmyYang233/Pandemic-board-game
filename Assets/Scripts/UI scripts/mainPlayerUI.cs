fe368aa4d49c6114654076f42c4c81ed7e9598f4
Dan Ning <ydn2012@hotmail.com>
2018-04-14 15:56:50 -0400

    Changing Field Operative

--- Assets/Scripts/UI scripts/mainPlayerUI.cs
+++ Assets/Scripts/UI scripts/mainPlayerUI.cs
@@ -8,6 +8,8 @@ public class mainPlayerUI : MonoBehaviour {
     public Button card;
     public GameObject cubeHolder;
     public eventCardUI eventCard;
+    public GameObject returnButtonPanel;
+    public Button[] buttons;
     private string redCube;
     private string blackCube;
     private string blueCube;
@@ -53,73 +55,89 @@ public class mainPlayerUI : MonoBehaviour {
             List<string> cubes = new List<string>();
             for(int i = 0; i<5; i++)
             {
-                Debug.Log(i + " has " + numberOfCubes[i]);
-                for (int j = 0; j < numberOfCubes[i]; i++)
+                if(numberOfCubes[i] == 0)
                 {
-                    
-                    if (i == 3)
+                    buttons[i].gameObject.GetComponent<Button>().interactable = false;
+                }
+                else
+                {
+                    buttons[i].gameObject.GetComponent<Button>().interactable = true;
+                    Debug.Log(i + " has " + numberOfCubes[i]);
+                    for (int j = 0; j < numberOfCubes[i]; i++)
                     {
-                        cubes.Add(blackCube);
-                    }
-                    else if (i == 1)
-                    {
-                        cubes.Add(redCube);
-                    }
-                    else if (i == 0)
-                    {
-                        cubes.Add(blueCube);
-                    }
-                    else
-                    {
-                        cubes.Add(yellowCube);
+                        if (i == 3)
+                        {
+                            cubes.Add(blackCube);
+                        }
+                        else if (i == 1)
+                        {
+                            cubes.Add(redCube);
+                        }
+                        else if (i == 0)
+                        {
+                            cubes.Add(blueCube);
+                        }
+                        else
+                        {
+                            cubes.Add(yellowCube);
+                        }
                     }
                 }
+                
             }
-            if (cubes.Count == 1)
+            if(cubes.Count == 0)
             {
-                GameObject cube = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
-                cube.transform.parent = cubeHolder.transform;
+                returnButtonPanel.SetActive(false);
             }
-            else if (cubes.Count == 2)
+            else
             {
-                GameObject cube1 = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
-                cube1.transform.parent = cubeHolder.transform;
-                GameObject cube2 = (GameObject)Instantiate(Resources.Load(cubes[1]), new Vector3(transform.position.x + 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
-                cube2.transform.parent = cubeHolder.transform;
-            }
-            else if (cubes.Count == 3)
-            {
-                GameObject cube1 = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
-                cube1.transform.parent = cubeHolder.transform;
-                GameObject cube2 = (GameObject)Instantiate(Resources.Load(cubes[1]), new Vector3(transform.position.x + 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
-                cube2.transform.parent = cubeHolder.transform;
-                GameObject cube3 = (GameObject)Instantiate(Resources.Load(cubes[2]), new Vector3(transform.position.x, transform.position.y - 6, transform.position.z), cubeHolder.transform.rotation);
-                cube3.transform.parent = cubeHolder.transform;
-            }
-            else if (cubes.Count == 4)
-            {
-                GameObject cube1 = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 6, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
-                cube1.transform.parent = cubeHolder.transform;
-                GameObject cube2 = (GameObject)Instantiate(Resources.Load(cubes[1]), new Vector3(transform.position.x + 6, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
-                cube2.transform.parent = cubeHolder.transform;
-                GameObject cube3 = (GameObject)Instantiate(Resources.Load(cubes[2]), new Vector3(transform.position.x - 6, transform.position.y - 6, transform.position.z), cubeHolder.transform.rotation);
-                cube3.transform.parent = cubeHolder.transform;
-                GameObject cube4 = (GameObject)Instantiate(Resources.Load(cubes[3]), new Vector3(transform.position.x + 6, transform.position.y - 6, transform.position.z), cubeHolder.transform.rotation);
-                cube4.transform.parent = cubeHolder.transform;
-            }
+                returnButtonPanel.SetActive(true);
+                if (cubes.Count == 1)
+                {
+                    GameObject cube = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube.transform.parent = cubeHolder.transform;
+                }
+                else if (cubes.Count == 2)
+                {
+                    GameObject cube1 = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube1.transform.parent = cubeHolder.transform;
+                    GameObject cube2 = (GameObject)Instantiate(Resources.Load(cubes[1]), new Vector3(transform.position.x + 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube2.transform.parent = cubeHolder.transform;
+                }
+                else if (cubes.Count == 3)
+                {
+                    GameObject cube1 = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube1.transform.parent = cubeHolder.transform;
+                    GameObject cube2 = (GameObject)Instantiate(Resources.Load(cubes[1]), new Vector3(transform.position.x + 7, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube2.transform.parent = cubeHolder.transform;
+                    GameObject cube3 = (GameObject)Instantiate(Resources.Load(cubes[2]), new Vector3(transform.position.x, transform.position.y - 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube3.transform.parent = cubeHolder.transform;
+                }
+                else if (cubes.Count == 4)
+                {
+                    GameObject cube1 = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 6, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube1.transform.parent = cubeHolder.transform;
+                    GameObject cube2 = (GameObject)Instantiate(Resources.Load(cubes[1]), new Vector3(transform.position.x + 6, transform.position.y + 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube2.transform.parent = cubeHolder.transform;
+                    GameObject cube3 = (GameObject)Instantiate(Resources.Load(cubes[2]), new Vector3(transform.position.x - 6, transform.position.y - 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube3.transform.parent = cubeHolder.transform;
+                    GameObject cube4 = (GameObject)Instantiate(Resources.Load(cubes[3]), new Vector3(transform.position.x + 6, transform.position.y - 6, transform.position.z), cubeHolder.transform.rotation);
+                    cube4.transform.parent = cubeHolder.transform;
+                }
 
-            else if (cubes.Count == 5)
-            {
-                GameObject cube1 = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 5, transform.position.y + 7, transform.position.z), cubeHolder.transform.rotation);
-                cube1.transform.parent = cubeHolder.transform;
-                GameObject cube2 = (GameObject)Instantiate(Resources.Load(cubes[1]), new Vector3(transform.position.x + 5, transform.position.y + 7, transform.position.z), cubeHolder.transform.rotation);
-                cube2.transform.parent = cubeHolder.transform;
-                GameObject cube3 = (GameObject)Instantiate(Resources.Load(cubes[2]), new Vector3(transform.position.x - 8, transform.position.y - 3, transform.position.z), cubeHolder.transform.rotation);
-                cube3.transform.parent = cubeHolder.transform;
-                GameObject cube4 = (GameObject)Instantiate(Resources.Load(cubes[3]), new Vector3(transform.position.x + 8, transform.position.y - 3, transform.position.z), cubeHolder.transform.rotation);
-                cube4.transform.parent = cubeHolder.transform;
-                GameObject cube5 = (GameObject)Instantiate(Resources.Load(cubes[4]), new Vector3(transform.position.x, transform.position.y - 10, transform.position.z), cubeHolder.transform.rotation);
-                cube5.transform.parent = cubeHolder.transform;
+                else if (cubes.Count == 5)
+                {
+                    GameObject cube1 = (GameObject)Instantiate(Resources.Load(cubes[0]), new Vector3(transform.position.x - 5, transform.position.y + 7, transform.position.z), cubeHolder.transform.rotation);
+                    cube1.transform.parent = cubeHolder.transform;
+                    GameObject cube2 = (GameObject)Instantiate(Resources.Load(cubes[1]), new Vector3(transform.position.x + 5, transform.position.y + 7, transform.position.z), cubeHolder.transform.rotation);
+                    cube2.transform.parent = cubeHolder.transform;
+                    GameObject cube3 = (GameObject)Instantiate(Resources.Load(cubes[2]), new Vector3(transform.position.x - 8, transform.position.y - 3, transform.position.z), cubeHolder.transform.rotation);
+                    cube3.transform.parent = cubeHolder.transform;
+                    GameObject cube4 = (GameObject)Instantiate(Resources.Load(cubes[3]), new Vector3(transform.position.x + 8, transform.position.y - 3, transform.position.z), cubeHolder.transform.rotation);
+                    cube4.transform.parent = cubeHolder.transform;
+                    GameObject cube5 = (GameObject)Instantiate(Resources.Load(cubes[4]), new Vector3(transform.position.x, transform.position.y - 10, transform.position.z), cubeHolder.transform.rotation);
+                    cube5.transform.parent = cubeHolder.transform;
+                }
             }
         }
     }
