4b595e60d194c14bcdafb39c87925882f26a947b
Dan Ning <ydn2012@hotmail.com>
2018-04-13 20:57:19 -0400

    Challenge selection over

---
+++ Assets/Scripts/UI scripts/challengeSelection.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class challengeSelection : MonoBehaviour {
+
+    public Button StartGame;
+    public Button HideRoom;
+    public Button LeaveRoom;
+    public Toggle VirulentStrainChallenge;
+    public Toggle MutationChallenge;
+    public Toggle BioTerroristChallenge;
+
+    private void Update()
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            StartGame.interactable = true;
+            HideRoom.interactable = true;
+            VirulentStrainChallenge.interactable = true;
+            MutationChallenge.interactable = true;
+            BioTerroristChallenge.interactable = true;
+        }
+        else
+        {
+            StartGame.interactable = false;
+            HideRoom.interactable = false;
+            VirulentStrainChallenge.interactable = false;
+            MutationChallenge.interactable = false;
+            BioTerroristChallenge.interactable = false;
+        }
+    }
+
+    public void MutationChallengeToggled()
+    {
+        if (MutationChallenge.isOn)
+        {
+            BioTerroristChallenge.isOn = false;
+        }
+    }
+
+    public void BioTerroristChallengeToggled()
+    {
+        if (BioTerroristChallenge.isOn)
+        {
+            MutationChallenge.isOn = false;
+        }
+       
+    }
+
+    public Challenge getChallenge()
+    {
+        if (VirulentStrainChallenge.isOn)
+        {
+            if (MutationChallenge.isOn)
+            {
+                return Challenge.MutationAndVirulentStrain;
+            }
+            else if (BioTerroristChallenge.isOn)
+            {
+                return Challenge.BioTerroistAndVirulentStrain;
+            }
+            else
+            {
+                return Challenge.VirulentStrain;
+            }
+        }
+        else
+        {
+            if (MutationChallenge.isOn)
+            {
+                return Challenge.Mutation;
+            }
+            else if (BioTerroristChallenge.isOn)
+            {
+                return Challenge.BioTerroist;
+            }
+            else
+            {
+                return Challenge.Nochallenge;
+            }
+        }
+
+    }
+
+}
