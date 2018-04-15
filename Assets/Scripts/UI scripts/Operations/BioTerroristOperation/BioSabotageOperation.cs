89b8f7ef088ce2d6c444fe234141e172c0317878
Dan Ning <ydn2012@hotmail.com>
2018-04-15 19:58:01 -0400

    small change

--- Assets/Scripts/UI scripts/Operations/BioTerroristOperation/BioSabotageOperation.cs
+++ Assets/Scripts/UI scripts/Operations/BioTerroristOperation/BioSabotageOperation.cs
@@ -6,9 +6,12 @@ using UnityEngine.UI;
 public class BioSabotageOperation : MonoBehaviour {
 
     public Game game;
+    public Button useSabotageButton;
+    public GameObject playerCardPanel;
     Player currentPlayer;
     City currentCity;
-    public Button useSabotageButton;
+
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,6 +21,35 @@ public class BioSabotageOperation : MonoBehaviour {
 
     public void sabotageButtonClicked()
     {
+        Color color = currentCity.getColor();
+        int num = playerCardPanel.transform.GetChild(1).childCount;
+        for (int i = 0; i < num; i++)
+        {
+            Debug.Log(i);
+            GameObject child = playerCardPanel.transform.GetChild(1).GetChild(i).gameObject;
+            string name = playerCardPanel.transform.GetChild(1).GetChild(i).GetChild(0).gameObject.GetComponent<Text>().text;
+            if (child.GetComponent<Image>().color == color)
+            {
+                child.GetComponent<Button>().interactable = true;
+                child.GetComponent<Button>().onClick.AddListener(() => sabotage(name));
+            }
+            else
+            {
+                child.GetComponent<Button>().interactable = false;
+            }
+        }
+    }
 
+    public void sabotage(string cardName)
+    {
+        int num = playerCardPanel.transform.GetChild(1).childCount;
+        for (int i = 0; i < num; i++)
+        {
+            Debug.Log(i);
+            GameObject child = playerCardPanel.transform.GetChild(1).GetChild(i).gameObject;
+            string name = playerCardPanel.transform.GetChild(1).GetChild(i).GetChild(0).gameObject.GetComponent<Text>().text;
+            child.GetComponent<Button>().interactable = false;
+            child.GetComponent<Button>().onClick.RemoveAllListeners();
+        }
     }
 }
