dd976f5688feeca594a9fcd7f9131cd161ca0a60
Meowdred <shuranzheng@shurandeMacBook-Pro.local>
2018-04-15 16:26:02 -0400

    lobby ui update

--- Assets/Scripts/UI scripts/Operations/OtherPlayerOperations/TroubleShooterOperationController.cs
+++ Assets/Scripts/UI scripts/Operations/OtherPlayerOperations/TroubleShooterOperationController.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TroubleShooterOperationController : MonoBehaviour {
 	public GameObject troublePanel;
+	public Game game;
+
 	// Use this for initialization
 	public void useSkill(){
 		troublePanel.SetActive (true);
+		List<string> infections = game.getInfectionDeckString ();
+		int rate = game.getInfectionRate ();
+		for (int i = 0; i < rate; i++) {
+			GameObject target = troublePanel.transform.GetChild (i).gameObject;
+			target.SetActive (true);
+			target.GetComponent<Image> ().color=game.findCity(infections[i]).getColor();
+			target.transform.GetChild (0).GetComponent<Text> ().text = infections [i];
+		}
+		for (int i = rate; i < 4; i++) {
+			troublePanel.transform.GetChild (0).gameObject.SetActive (false);
+		}
 	}
 }
