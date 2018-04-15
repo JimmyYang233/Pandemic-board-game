cba3ce2ab8095d628526005a3c07d98f7f096db0
Dan Ning <ydn2012@hotmail.com>
2018-04-15 17:31:31 -0400

    BioStillWorking

---
+++ Assets/Scripts/UI scripts/Operations/BioTerroristOperation/BioInfectOperation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BioInfectOperation : MonoBehaviour {
+
+    public Game game;
+    public Button infectLocallyButton;
+    public Button ifectRemotellyButton;
+    public Button cancelButton;
+    Player currentPlayer;
+    City currentCity;
+
+    public void infectButtonClicked()
+    {
+        currentPlayer = game.getCurrentPlayer();
+        currentCity = currentPlayer.getPlayerPawn().getCity();
+        infectLocallyButton.gameObject.GetComponent<Button>().interactable = true;
+    }
+}
