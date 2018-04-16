54b261aaf1f037dc901442b1c7d8730a7f5c12ea
Zhening <taylor_fantasy@163.com>
2018-04-16 05:23:01 -0400

    add

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -766,6 +766,7 @@ public class Game : MonoBehaviour {
 
 
 		//wining saved game
+		/*
 		foreach(City c in cities)
 		{
 			playerCardDeck.Add(new CityCard(c));
@@ -776,11 +777,11 @@ public class Game : MonoBehaviour {
 		foreach (EventKind k in eventKinds)
 		{
 			playerCardDeck.Add(EventCard.getEventCard(k));
-		}
+		}*/
 
 
 		//eventcardsavedgame
-		/*
+
 		List<EventKind> eventKinds = mapInstance.getEventNames();
 		foreach (EventKind k in eventKinds)
 		{
@@ -791,7 +792,7 @@ public class Game : MonoBehaviour {
 		{
 			playerCardDeck.Add(new CityCard(c));
 			infectionDeck.Add(new InfectionCard(c));
-		}*/
+		}
 
 		if(challenge == Challenge.Mutation || challenge == Challenge.MutationAndVirulentStrain){
 			for(int i = 0; i<2; i++){
